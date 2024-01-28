060dc34ddd8febdd4008d2abbaff2f19aa5367c8
Prakash Chatlani <chatlanipr@gmail.com>
2024-01-28 11:15:15 -0500

    ggfgtfd

--- js/server.js
+++ js/server.js
@@ -8,7 +8,7 @@ const wss = new WebSocket.Server({ server: server });
 
 wss.on('connection', function connection(ws) {
     console.log('A new client Connected!');
-    ws.send('Welcome New Client!');
+    // ws.send('Welcome New Client!');
 
     ws.on('message', function incoming(message) {
         console.log(message);
@@ -23,13 +23,14 @@ wss.on('connection', function connection(ws) {
                 return;
             }
             console.log(`stdout: ${stdout}`);
+            ws.send(stdout)
         });
 
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
+        // wss.clients.forEach(function each(client) {
+        //     if (client !== ws && client.readyState === WebSocket.OPEN) {
+        //         client.send(message);
+        //     }
+        // });
 
     });
 });
