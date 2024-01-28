40352dda23027f0d8d8250acd6d81f7ebeab9227
Gokul Chaluvadi <gokul.chaluvadi@gmail.com>
2024-01-28 11:11:40 -0500

    Update

--- js/hideHeroMain.js
+++ js/hideHeroMain.js
@@ -1,6 +1,10 @@
 function hideHeroMain() {
   var heroMain = document.querySelector('.hero-main');
   var heroTools = document.querySelector('.hero-tools');
+  var duplicateHeroTools = document.querySelector('.hero-tools-duplicate');
+  var heroContainer = document.querySelector('.hero-container');
+
+
 
   // Hero Main
   heroMain.style.opacity = 0;
@@ -12,6 +16,17 @@ function hideHeroMain() {
 
   // Slide the image to the left with a smoother transition and zoom effect
   var previewImage = document.getElementById('previewImage');
+
+  heroTools.style.opacity = 0;
+  heroTools.style.transition = 'opacity 0.5s ease';
+  
+
+  setTimeout(function () {
+    heroTools.style.opacity = 100;
+    heroTools.style.transition = 'opacity 0.5s ease';
+  }, 700);
+  
+
   var duration = 1500;  // Adjust the duration here
   var startTime = null;
   var startMargin = 0; // Adjust the starting margin
@@ -26,7 +41,7 @@ function hideHeroMain() {
 
     previewImage.style.marginLeft = -startMargin * easedProgress + 'px';
     previewImage.style.transform = 'scale(' + (startScale + (7 - startScale) * easedProgress) + ')'; // Adjust the target scale value
-
+    
     if (progress < 1) {
       requestAnimationFrame(animate);
     }
@@ -38,4 +53,11 @@ function hideHeroMain() {
 // Easing function for smoother animation
 function easeOutCubic(t) {
   return 1 - Math.pow(1 - t, 3);
+}
+
+function showDuplicateHeroTools() {
+  // Append the duplicate element to the existing flex container
+  heroContainer.appendChild(duplicateHeroTools);
+  
+  duplicateHeroTools.style.display = 'flex'; // Or 'block' depending on the original display property
 }
